f47d3bb3f3336324731e55ef9cc921d255ed2aae
murselkaradas <72932630+murselkaradas@users.noreply.github.com>
2024-06-18 21:56:17 -0400

    Update RUN_stim_prepocess.m

--- RUN_stim_prepocess.m
+++ RUN_stim_prepocess.m
@@ -9,25 +9,45 @@ set(0,'defaultfigurecolor',[1 1 1])
 set(0,'DefaultAxesTitleFontWeight', 'normal')
 set(0,'DefaultTextInterpreter','none')
 
-addpath(genpath('/gpfs/data/rinberglab/Mursel/ImagingPreProcess/'))
+addpath(genpath('/gpfs/scratch/karadm01/ImagingPreProcess'))
 %%
 
-folderpath = '/gpfs/data/rinberglab/Mursel/240520';
-roifile ='RoiSet';
-
-
-fieldname = 'JG41851_240520_field1_00001';
-VoyeurH5_file = 'JG41851_240520_field1_1_01_D2024_5_20T17_57_7_odor';
+folderpath = '/gpfs/scratch/karadm01/MultiScaleMapping/240613';
+roifile ='20240613_RoiSet';
 WSfieldname = 'OMP02_230207_HighConcWS';
 
-get_prepocessed_stimdata('tiffpath',folderpath, 'fieldname', fieldname, 'roiname', roifile,'VoyeurH5_name', VoyeurH5_file,'isOdor',false,'calculate_diff_image',false,...
-   'isWS',false, 'inh_realign',false,'pre',1000,'post',2000,'usealigned_tiff',false)
+fieldname = 'JG11571_240613_field1_2xDMD2P_DMDstim_00001';
+VoyeurH5_file = '11571_1_01_D2024_6_13T17_33_31_odor';
 
-%%
-%fieldname = 'JG44524_230614_glomfield4_seq_00001';
-%VoyeurH5_file = 'JG44524_230614_glomfield4_seq_1_01';
-%get_prepocessed_stimdata('tiffpath',folderpath, 'fieldname', fieldname, 'roiname', roifile,'VoyeurH5_name', VoyeurH5_file,'isOdor',false)
+get_prepocessed_stimdata('tiffpath',folderpath, 'fieldname', fieldname, 'roiname', roifile,'VoyeurH5_name', VoyeurH5_file,'isOdor',false,'calculate_diff_image',true,...
+   'isWS',false, 'inh_realign',false,'pre',1000,'post',2000,'usealigned_tiff',true)
 
-%fieldname = 'JG44524_230614_glomfield4_seq_00002';
-%VoyeurH5_file = 'JG44524_230614_glomfield4_seq_1_02';
-%get_prepocessed_stimdata('tiffpath',folderpath, 'fieldname', fieldname, 'roiname', roifile,'VoyeurH5_name', VoyeurH5_file,'isOdor',false)
+
+fieldname = 'JG11571_240613_field1_2xDMD2P_DMDstim_00002';
+VoyeurH5_file = '11571_1_02_D2024_6_13T18_22_23_odor';
+get_prepocessed_stimdata('tiffpath',folderpath, 'fieldname', fieldname, 'roiname', roifile,'VoyeurH5_name', VoyeurH5_file,'isOdor',false,'calculate_diff_image',true,...
+   'isWS',false, 'inh_realign',false,'pre',1000,'post',2000,'usealigned_tiff',true)
+
+
+fieldname = 'JG11571_240613_field1_2xSLM2P_cellseries_00001';
+VoyeurH5_file = '11571_1_01_D2024_6_13T20_46_10_odor';
+get_prepocessed_stimdata('tiffpath',folderpath, 'fieldname', fieldname, 'roiname', roifile,'VoyeurH5_name', VoyeurH5_file,'isOdor',false,'calculate_diff_image',true,...
+   'isWS',false, 'inh_realign',false,'pre',1000,'post',2000,'usealigned_tiff',true)
+
+
+fieldname = 'JG11571_240613_field1_2xSLM2P_cellseries_00002';
+VoyeurH5_file = '11571_1_02_D2024_6_13T21_23_27_odor';
+get_prepocessed_stimdata('tiffpath',folderpath, 'fieldname', fieldname, 'roiname', roifile,'VoyeurH5_name', VoyeurH5_file,'isOdor',false,'calculate_diff_image',true,...
+   'isWS',false, 'inh_realign',false,'pre',1000,'post',2000,'usealigned_tiff',true)
+
+
+fieldname = 'JG11571_240613_field1_2xSLM2P_cellseries_00003';
+VoyeurH5_file = '11571_1_03_D2024_6_13T21_47_7_odor';
+get_prepocessed_stimdata('tiffpath',folderpath, 'fieldname', fieldname, 'roiname', roifile,'VoyeurH5_name', VoyeurH5_file,'isOdor',false,'calculate_diff_image',true,...
+   'isWS',false, 'inh_realign',false,'pre',1000,'post',2000,'usealigned_tiff',true)
+
+
+fieldname = 'JG11571_240613_field1_2xSLM2P_cellseries_00004';
+VoyeurH5_file = '11571_1_04_D2024_6_13T22_39_1_odor';
+get_prepocessed_stimdata('tiffpath',folderpath, 'fieldname', fieldname, 'roiname', roifile,'VoyeurH5_name', VoyeurH5_file,'isOdor',false,'calculate_diff_image',true,...
+   'isWS',false, 'inh_realign',false,'pre',1000,'post',2000,'usealigned_tiff',true)
